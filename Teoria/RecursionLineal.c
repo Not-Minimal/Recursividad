583b489c128ee890cf53735b074f192e0d0daf8d
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-09 20:04:07 -0300

    Recursividad Lineal

---
+++ Teoria/RecursionLineal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int factorial(int numero){
+ if (numero > 1) return (numero*factorial(numero-1));
+ else return(1);
+}
+int main (){
+ int n;
+ printf("Introduce el número: ");
+ scanf("%d",&n);
+ printf("El factorial es %d\n", factorial(n));
+}
